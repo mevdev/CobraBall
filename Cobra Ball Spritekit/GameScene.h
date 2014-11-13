97dbe25f6243292ee89bdc2ceaa67e5fd0464d3c
Robert Linnemann <robertlinnemann@gmail.com>
2014-11-12 21:03:31 -0800

    Initial Commit

---
+++ Cobra Ball Spritekit/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Cobra Ball Spritekit
+//
+
+//  Copyright (c) 2014 Robert Linnemann. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Cobra Ball Spritekit/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Cobra Ball Spritekit
+//
+
+//  Copyright (c) 2014 Robert Linnemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
